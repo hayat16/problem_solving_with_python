aa0c86201f5d9f3c66a710f7bb523e1759dea611
Mysha Sarin <35870219+hayat16@users.noreply.github.com>
2022-07-13 00:20:23 +0600

    Create problem_3.py

---
+++ problem_3.py
@@ -0,0 +1,12 @@
+Write a program which prompts the user for the height and width of a rectangle, calculate the area, diagonal, and perimeter.
+
+import math
+height=float(input('Enter Height: '))
+width=float(input('Enter Width: '))
+area=height*width
+Perimeter=2*(height+width)
+Diagonal=2*math.sqrt(pow(height,2)+pow(width,2))
+print(f'Area : {area}')
+print(f'Perimete : {Perimeter}')
+print(f'Diagonal : {Diagonal}')
+print('Diagonal : %.4f'%Diagonal)

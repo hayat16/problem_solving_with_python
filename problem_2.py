aac5d64388b4a71706a03da32aa495993e73954a
Mysha Sarin <35870219+hayat16@users.noreply.github.com>
2022-07-13 00:18:44 +0600

    Create problem_2.py

---
+++ problem_2.py
@@ -0,0 +1,6 @@
+Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit, and print out the converted temperature.
+
+C=float(input('Enter Tempatature in Celsius: '))
+F=(C*1.8)+32
+print(f'Temparature in Farenheit: {F}')
+print('Temparature in Farenheit %.4f'%F)

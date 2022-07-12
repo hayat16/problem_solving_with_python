52f9995cd5b267ff532665e4a9bbac1125c2c015
Mysha Sarin <35870219+hayat16@users.noreply.github.com>
2022-07-13 00:22:27 +0600

    Create problem_5.py

---
+++ problem_5.py
@@ -0,0 +1,10 @@
+Write a Python program to calculate surface volume and area of a cylinder.
+
+import math
+h=float(input('  Enter Height: '))
+r=float(input('  Enter radius: '))
+p=math.pi
+volume=p*pow(r,2)*h
+area=(2*p*r*h)+(2*p*pow(r,2))
+print(f'Volume: {volume} & Area {area}')
+print('Volume: {:.4f} & Area {:.4f}'.format(volume,area))
